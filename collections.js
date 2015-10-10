07d682dd416239fc610998a485c5fccc2209167e
Viktor Gustafsson <viktorgustafsson92@gmail.com>
2015-10-10 20:43:59 +0900

    Changed location for collection creation

---
+++ collections.js
@@ -0,0 +1,9 @@
+Rooms = new Mongo.Collection("rooms", {
+  // For future use, for example if we want to create hard relations
+})
+Positions = new Mongo.Collection("positions", {
+
+})
+Users = new Mongo.Collection("users", {
+
+})
