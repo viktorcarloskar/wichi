03ed504e50c9907865a8d4c40ae76d96f650f5b9
johanhenrikssn <johan.c.henriksson@gmail.com>
2015-10-10 18:03:00 +0900

    Added map

--- client/client.js
+++ client/client.js
@@ -1,9 +1,9 @@
-Rooms = new mongo.collection("rooms")
-Positions = new mongo.collection("positions")
+//Rooms = new Mongo.Collection("rooms")
+//Positions = new Mongo.Collection("positions")
 
-if (meteor.isClient) {
+if (Meteor.isClient) {
 
-  Template.main.helpers({
+  /*Template.main.helpers({
     rooms: function() {
       return Rooms.find();
     }
@@ -13,5 +13,32 @@ if (meteor.isClient) {
     locations: function() {
       return Positions.find();
     }
-  })
+  })*/
+  Meteor.startup(function() {
+    GoogleMaps.load();
+  });
+
+  Template.body.helpers({
+    exampleMapOptions: function() {
+      // Make sure the maps API has loaded
+      if (GoogleMaps.loaded()) {
+        // Map initialization options
+        return {
+          center: new google.maps.LatLng(-37.8136, 144.9631),
+          zoom: 8
+        };
+      }
+    }
+  });
+
+  Template.body.onCreated(function() {
+    // We can use the `ready` callback to interact with the map API once the map is ready.
+    GoogleMaps.ready('exampleMap', function(map) {
+      // Add a marker to the map once it's ready
+      var marker = new google.maps.Marker({
+        position: map.options.center,
+        map: map.instance
+      });
+    });
+  });
 }
