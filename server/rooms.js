e275197140c3025c84f76aa3cd1f2590e70553d7
Viktor Gustafsson <viktorgustafsson92@gmail.com>
2015-10-11 10:42:22 +0900

    Made it possible to make users active again

--- server/rooms.js
+++ server/rooms.js
@@ -5,9 +5,15 @@ if (Meteor.isServer) {
   Meteor.setInterval(function() {
     var inactiveDate = new Date();
     inactiveDate.setMinutes(inactiveDate.getMinutes() - 1)
-    var users = Users.find({lastActive: {$lt: inactiveDate}}).forEach(function(user) {
+    // Handle inactive users
+    Users.find({lastActive: {$lt: inactiveDate}}).forEach(function(user) {
       Users.update(user._id, {active: false})
       console.log("Made the user: " + user.userId + " inactive");
+    })
+    // Handle users that becomes active again
+    var users = Users.find({lastActive: {$gte: inactiveDate}, active: false}).forEach(function(user) {
+      Users.update(user._id, {active: true})
+      console.log("Made the user: " + user.userId + " active again");
     })
   }, 5000)
 }
